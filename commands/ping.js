75741ad10ee3d83e64c2e1960958232ab67919b2
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-01-06 18:56:09 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,17 +1,17 @@
 //---------------------------------------------------------------------------
 const Secktor = require('../lib')
 Secktor.cmd({
-        pattern: "check",
+        pattern: "ping",
         desc: "To check ping",
         category: "general",
         react: "🗿",
         filename: __filename,
     },
-    async(Void, citel) => {
+    async(Void, citel, text) => {
         var inital = new Date().getTime();
         const { key } = await Void.sendMessage(citel.chat, {text: '```Checking Ping!```'});
         var final = new Date().getTime();
        await Secktor.sleep(1000)
-       return await Void.sendMessage(citel.chat, {text: '*Pong*\n *' + (final - inital) + ' ms* ', edit: key});
+       return await Void.sendMessage(citel.chat, {text: '*Pong' + (final - inital) + 'ms*', edit: key});
     }
 );
